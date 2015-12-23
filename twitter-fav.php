694fd1d2f1eb509db8b0555250656397e3786c8f
a1yama <a1yama1123@gmail.com>
2015-12-23 14:38:43 +0900

    重複しているものは除くように修正

--- twitter-fav.php
+++ twitter-fav.php
@@ -1,34 +1,56 @@
 <?php
-    require_once('twitteroauth/autoload.php');
-    require_once('twitteroauth/src/TwitterOAuth.php');
-    require_once(dirname(__FILE__) . '/config.php');
-    use Abraham\TwitterOAuth\TwitterOAuth;
 
-    $connection = new TwitterOAuth(consumer_key, consumer_secret, access_token, access_token_secret);
-    
-    $keyword_list = array(
-        "ANIERA",
-    );
-    foreach ( $keyword_list as $keyword ) {
-        $statues = $connection->get("search/tweets", array(
-            "q"                 => $keyword.' -from:@Aniera_Japan -from:@Gunshi320 exclude:retweets',
-            "count"             => "10",
-            "include_entities"  => "false",
-            "lang"              => "ja",
-            "locale"            => "ja",
-            "result_type"       => "recent",
+require_once('twitteroauth/autoload.php');
+require_once('twitteroauth/src/TwitterOAuth.php');
+require_once(dirname(__FILE__) . '/config.php');
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+$connection = new TwitterOAuth(consumer_key, consumer_secret, access_token, access_token_secret);
+
+$fileName = __DIR__ . "/data/fav.db";
+
+$favDatas = file_get_contents($fileName);
+
+$favDataArray = explode(',', $favDatas);
+
+$keyword_list = array(
+    "aniera",
+);
+$fav_list = [];
+foreach ( $keyword_list as $keyword ) {
+    $statues = $connection->get("search/tweets", array(
+        "q"                 => $keyword.' -from:@Aniera_Japan -from:@Gunshi320 exclude:retweets',
+        "count"             => "10",
+        "include_entities"  => "false",
+        "lang"              => "ja",
+        "locale"            => "ja",
+        "result_type"       => "recent",
+    ));
+
+    foreach ($statues->statuses as $list) {
+        $favFlg = true;
+        foreach ($favDataArray as $oldFav) {
+            if ($oldFav == @$list->id) {
+                $favFlg = false;
+            }
+        }
+        if ($favFlg) {
+            $fav_list[] = @$list->id;
+        }
+    }
+}
+
+if (isset($fav_list)) {
+    foreach ($fav_list as $value) {
+        $statues = $connection->post("favorites/create", array(
+            "id" => $value,
+            "include_entities" => "false",
         ));
-    
-        foreach ($statues->statuses as $list) {
-            $favo_list[] = @$list->id;
-        }
     }
+}
 
-    if (isset($favo_list)) {
-        foreach ($favo_list as $value) {
-            $statues = $connection->post("favorites/create", array(
-                "id" => $value,
-                "include_entities" => "false",
-            ));
-        }
-    }
+$fav = '';
+foreach ($fav_list as $item) {
+    $fav .= ',' . $item;
+}
+file_put_contents($fileName, $fav, FILE_APPEND);
