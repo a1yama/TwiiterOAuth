c269074ba54f69908dbc021e666c1cb009d17104
a1yama <a1yama1123@gmail.com>
2016-01-14 18:09:19 +0900

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define("consumer_key", "nRhV58Pl1xuILEjY8p4XYfJwy");
-define('consumer_secret', 'Yf9e1Q13mYqlZ8YI6EAiC2L1paHj9wZTTMp8tpgQDTicW0T6GB');
-define('access_token', '485416793-tG6Dal28KpCXCi9OFW3QRuYjWzQBNb4uCfkyxMlN');
-define('access_token_secret', 'b1hTr9ChaXV14Xxavl2aAVfFMQl9pVcxLoNaqtp4mGIcg');
-
-define('api_url', 'http://localhost:5555/twitter/api/post');
