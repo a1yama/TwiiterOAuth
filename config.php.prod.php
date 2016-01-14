13459bc43408e51d8e368eda627c8da3c864f195
a1yama <a1yama1123@gmail.com>
2016-01-14 17:54:12 +0900

    config変更

--- config.php.prod.php
+++ config.php.prod.php
@@ -1,7 +1,7 @@
 <?php
-define("consumer_key", "nRhV58Pl1xuILEjY8p4XYfJwy");
-define('consumer_secret', 'Yf9e1Q13mYqlZ8YI6EAiC2L1paHj9wZTTMp8tpgQDTicW0T6GB');
-define('access_token', '485416793-tG6Dal28KpCXCi9OFW3QRuYjWzQBNb4uCfkyxMlN');
-define('access_token_secret', 'b1hTr9ChaXV14Xxavl2aAVfFMQl9pVcxLoNaqtp4mGIcg');
+define("consumer_key", "mVbgKTwmhohzq4tUVH9kWNWLs");
+define('consumer_secret', 'qgB5yc3ouyK4Q8RMZChGcefS6kBJ0L8Hjy5MqlSbGGZpM00fx6');
+define('access_token', '1013547038-z37uNTT0pv32m7zppZgXMgOXEet6QVnSLshEQgd');
+define('access_token_secret', 'iGttNeIxhXRjSeLvJDShwbQfT92MO8ICz7j5bSM7tskY3');
 
 define('api_url', 'http://aniera.a1yama.com/twitter/api/post');
