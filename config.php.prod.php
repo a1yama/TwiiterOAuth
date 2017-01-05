92d086bc2fab482d813a46bae327aa14a603102b
a1yama <a1yama1123@gmail.com>
2017-01-05 16:41:48 +0900

    config整理

--- config.php.prod.php
+++ config.php.prod.php
@@ -1,9 +1,9 @@
 <?php
-define("consumer_key", "mVbgKTwmhohzq4tUVH9kWNWLs");
-define('consumer_secret', 'qgB5yc3ouyK4Q8RMZChGcefS6kBJ0L8Hjy5MqlSbGGZpM00fx6');
-define('access_token', '1013547038-z37uNTT0pv32m7zppZgXMgOXEet6QVnSLshEQgd');
-define('access_token_secret', 'iGttNeIxhXRjSeLvJDShwbQfT92MO8ICz7j5bSM7tskY3');
+define("consumer_key", "test");
+define('consumer_secret', 'test');
+define('access_token', '1013547038-test');
+define('access_token_secret', 'test');
 
-define('api_url', 'http://aniera.a1yama.com/twitter/api/post');
+define('api_url', 'http://example.com/api');
 
-define('SLACK_API_KEY', 'xoxp-10189968451-10190149879-16888105506-8c17be108b');
+define('SLACK_API_KEY', 'xoxp-test');
